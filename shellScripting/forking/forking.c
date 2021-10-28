6f5bc5dc6f6b96bbe5499c2651f53a6e3e7fd4f2
keder-code-hash <kedernath.mallick.tint022@gmail.com>
2021-10-28 15:26:21 +0530

    forking

---
+++ shellScripting/forking/forking.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+int main(void){
+	int ret=fork();
+	if(ret ==0){
+		printf("child is created Successfully\n");
+		int parentPid=getppid();
+		int childPid=getpid();
+		printf("CHILD_PROCESS_PID:-%d\nPARENT_PROCESS_PID:-%d\n",childPid,parentPid );
+	}
+	else if(ret<0){
+		printf("unsuccessfull!");
+	}
+	else{
+		int parentPID=getppid();
+		int currentPID=getpid();
+		printf("CURRENT_PROCESS_PID:-%d\nPARENT_PROCESS_PID:-%d\n",currentPID,parentPID );   
+	}
+	return 0;
+}
